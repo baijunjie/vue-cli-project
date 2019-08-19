3ff279f6eddfa9bfdbb318f865d65e4b8010b21f
baijunjie <bjj19851010@gmail.com>
2019-08-19 12:09:01 +0900

    update

--- src/core/ui.js
+++ src/core/ui.js
@@ -12,6 +12,8 @@ import ElementUI from 'element-ui'
 //   // set './EsSwitch.vue'       => 'EsSwitch'
 //   // set './EsSwitch/index.vue' => 'EsSwitch'
 //   const moduleName = modulePath.replace(/^\.\/(.*?)(\/index)?\.\w+$/, '$1')
+//   // Shielding child components
+//   if (moduleName.indexOf('/') >= 0) return
 //   const value = modulesFiles(modulePath)
 //   Vue.component(moduleName, value.default)
 // })
